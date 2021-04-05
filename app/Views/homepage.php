b65d9fa7616fd527dde6c9e7602488d1ba131d0b
Jordane-Kraken <jordane.minaya@gmail.com>
2021-04-05 16:34:26 +0200

    homepage wip

---
+++ app/Views/homepage.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pokedex</title>
+	<meta name="description" content="The small framework with powerful features">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+	<link href="assets/css/style.css" rel="stylesheet">
+
+	<!-- STYLES -->
+</head>
+<body>
+
+<!-- HEADER: MENU + HEROE SECTION -->
+<header class='header'>
+	<img class='pokeball' src='assets/images/pokeball.png'/>
+		<h1 class='title'>Pokedex made with Code Igniter 4</h1>
+	<img class='pokeball' src='assets/images/pokeball.png'/>
+</header>
+
+<!-- CONTENT -->
+
+<section>
+
+</section>
+
+
+<!-- FOOTER: DEBUG INFO + COPYRIGHTS -->
+
+<footer>
+	<div>
+		<p>Pokedex - 2021 </p>
+	</div>
+</footer>
+
+<!-- SCRIPTS -->
+
+<!-- -->
+
+</body>
+</html>
